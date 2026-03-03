863e39d059c81cda6b2fa7d047bfc1fb6b7eff2d
Tayná <tayna.stoski@escola.pr.gov.br>
2026-03-03 15:25:30 +0000

    .

--- type-number.js
+++ type-number.js
@@ -2,3 +2,11 @@
 const meuNumero = 3;
 const primeiroNumero = 1;
 const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemzero = .5;
